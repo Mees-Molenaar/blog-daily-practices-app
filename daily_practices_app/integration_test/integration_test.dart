678e9719d3bccda0b46cde10adb48fe2dedd1715
Mees Molenaar <108014167+MeesMolenaar@users.noreply.github.com>
2023-01-07 15:33:15 +0100

    feat: add integration test
    The integration test will fail.
    There only is a placeholder for the app in app.dart.

---
+++ daily_practices_app/integration_test/integration_test.dart
@@ -0,0 +1,57 @@
+import 'package:daily_practices_app/app/app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('all practices are in the list and one is active',
+        (tester) async {
+      await tester.pumpWidget(
+        const DailyPracticeApp(),
+      );
+
+      // Wait till app is loaded
+      await Future.delayed(const Duration(seconds: 3), () {});
+
+      // Verify that the list can be found
+      final listFinder = find.byType(Scrollable);
+      expect(listFinder, findsOneWidget);
+
+      // Verify that the first practice can be found
+      expect(find.text('Sleep eight hours'), findsOneWidget);
+
+      // Scroll to the bottom
+      await tester.fling(
+        listFinder,
+        const Offset(0, -500),
+        10000,
+      );
+      await tester.pumpAndSettle();
+
+      // Verify that the last practice can be found
+      expect(find.text('Deep breathing'), findsOneWidget);
+
+      // Scroll back to the top
+      await tester.fling(
+        listFinder,
+        const Offset(0, 500),
+        10000,
+      );
+      await tester.pumpAndSettle();
+
+      final activeItemFinder = find.byKey(const ValueKey('ActivePractice'));
+
+      // Find the active practice
+      await tester.scrollUntilVisible(
+        activeItemFinder,
+        500.0,
+        scrollable: listFinder,
+      );
+
+      expect(activeItemFinder, findsOneWidget);
+    });
+  });
+}
