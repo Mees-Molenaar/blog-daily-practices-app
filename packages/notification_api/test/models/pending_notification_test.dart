aa1a65ec49cd1b33ed2cb8f92e156fbb60d9e3d7
Mees Molenaar <108014167+MeesMolenaar@users.noreply.github.com>
2023-07-23 10:09:40 +0200

    fix: change to right import

--- packages/notification_api/test/models/pending_notification_test.dart
+++ packages/notification_api/test/models/pending_notification_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:notification_api/src/models/active_notification.dart';
+import 'package:notification_api/notification_api.dart';
 
 void main() {
   group('PendingNotification', () {
